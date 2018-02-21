c3e0ee55f5be2fdabdb6e868c10945af2f751830
Nathan Rajlich <nathan@tootallnate.net>
2018-02-20 18:28:35 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const Readable = require('stream');
+const readline = require('readline');
+
+module.exports = createTtyWrite;
+
+function createTtyWrite(stream = process.stdout, options = {}) {
+  const input = new Readable();
+  input.resume = input.pause = () => {};
+  const rl = readline.createInterface(
+    Object.assign({}, options, {
+      input,
+      output: stream
+    })
+  );
+
+  const write = rl.write;
+  rl.write = function(v) {
+    let s, key;
+    if (typeof v === 'string') {
+      s = v;
+      key = null;
+    } else {
+      s = v.sequence;
+      key = v;
+    }
+    return write.call(this, s, key);
+  };
+
+  return rl;
+}
